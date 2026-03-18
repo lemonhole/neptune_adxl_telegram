a2617002ca415d942abb4abed7d12a9f56807d8a
lemonhole <96912372+lemonhole@users.noreply.github.com>
2026-03-18 15:35:45 +0300

    Update shaper_calibrate.sh
    stripped coordinates from output filenames for better readability

--- shaper_calibrate.sh
+++ shaper_calibrate.sh
@@ -17,7 +17,9 @@ if [ "$#" -gt 0 ]
 then
     for each_file in resonances*.csv
     do
-        $KLIPPER_SCRIPTS_LOCATION/calibrate_shaper.py $each_file -o /$PRINTER_DATA/$OUTPUT_FOLDER/${each_file%.csv}.png # check
+        CLEAN_NAME=$(echo "$each_file" | cut -d'_' -f1,2,6,7)
+        PNG_NAME="${CLEAN_NAME%.csv}.png"
+        $KLIPPER_SCRIPTS_LOCATION/calibrate_shaper.py "$each_file" -o "/$PRINTER_DATA/$OUTPUT_FOLDER/$PNG_NAME"
         rm /$RESONANCE_CSV_LOCATION/$each_file
     done
 else
