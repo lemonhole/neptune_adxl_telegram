277f0fe11f14dc07bbae932940976ca553fe574d
lemonhole <96912372+lemonhole@users.noreply.github.com>
2026-03-20 10:32:00 +0300

    Update shaper_calibrate.sh
    fix

--- shaper_calibrate.sh
+++ shaper_calibrate.sh
@@ -1,30 +1,24 @@
-#! /bin/bash
-OUTPUT_FOLDER=config/adxl_results
-PRINTER_DATA=$HOME/printer_data
-KLIPPER_SCRIPTS_LOCATION=~/klipper/scripts
-RESONANCE_CSV_LOCATION=tmp
-if [ ! -d  /$PRINTER_DATA/$OUTPUT_FOLDER/ ] #Check if we have an output folder
-then
-    mkdir /$PRINTER_DATA/$OUTPUT_FOLDER/
-fi
+#!/bin/bash
+OUTPUT_FOLDER="config/adxl_results"
+PRINTER_DATA="$HOME/printer_data"
+KLIPPER_SCRIPTS_LOCATION="$HOME/klipper/scripts"
+RESONANCE_CSV_LOCATION="/tmp"
 
-cd /$RESONANCE_CSV_LOCATION/
+mkdir -p "$PRINTER_DATA/$OUTPUT_FOLDER"
+cd "$RESONANCE_CSV_LOCATION" || exit
 
 shopt -s nullglob
 set -- resonances*.csv
 
-if [ "$#" -gt 0 ]
-then
-    for each_file in resonances*.csv
-    do
-        CLEAN_NAME=$(echo "$each_file" | cut -d'_' -f1,2,6,7)
-        HISTORY_NAME="${CLEAN_NAME%.csv}.png"
-        STATIC_NAME="resonances_x.png"
+if [ "$#" -gt 0 ]; then
+    for each_file in resonances*.csv; do
+        HISTORY_NAME="$(echo "${each_file%.csv}" | cut -d'_' -f1,2,6,7).png"
+        STATIC_NAME="${each_file:0:12}.png"
 
-        $KLIPPER_SCRIPTS_LOCATION/calibrate_shaper.py "$each_file" -o "$PRINTER_DATA/$OUTPUT_FOLDER/$STATIC_NAME"
+        "$KLIPPER_SCRIPTS_LOCATION/calibrate_shaper.py" "$each_file" -o "$PRINTER_DATA/$OUTPUT_FOLDER/$STATIC_NAME"
         cp "$PRINTER_DATA/$OUTPUT_FOLDER/$STATIC_NAME" "$PRINTER_DATA/$OUTPUT_FOLDER/$HISTORY_NAME"
-        rm "$RESONANCE_CSV_LOCATION/$each_file"
+        rm "$each_file"
     done
 else
-    echo "Something went wrong, no csv found to process"
+    echo "No CSV files found in $RESONANCE_CSV_LOCATION"
 fi
