d7a757c244128592c8811513a92254d175c7d040
lemonhole <96912372+lemonhole@users.noreply.github.com>
2026-03-20 10:41:17 +0300

    Update shaper_calibrate.sh
    добавлена папка для отправки боту с перезаписываемым файлом

--- shaper_calibrate.sh
+++ shaper_calibrate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-OUTPUT_FOLDER="config/adxl_results"
 PRINTER_DATA="$HOME/printer_data"
+OUTPUT_FOLDER="$PRINTER_DATA/config/adxl_results"
+BOT_FOLDER="$OUTPUT_FOLDER/bot"
 KLIPPER_SCRIPTS_LOCATION="$HOME/klipper/scripts"
 RESONANCE_CSV_LOCATION="/tmp"
 
-mkdir -p "$PRINTER_DATA/$OUTPUT_FOLDER"
+mkdir -p "$BOT_FOLDER"
 cd "$RESONANCE_CSV_LOCATION" || exit
 
 shopt -s nullglob
@@ -12,13 +13,13 @@ set -- resonances*.csv
 
 if [ "$#" -gt 0 ]; then
     for each_file in resonances*.csv; do
-        HISTORY_NAME="$(echo "${each_file%.csv}" | cut -d'_' -f1,2,6,7).png"
         STATIC_NAME="${each_file:0:12}.png"
+        HISTORY_NAME="$(echo "${each_file%.csv}" | cut -d'_' -f1,2,6,7).png"
 
-        "$KLIPPER_SCRIPTS_LOCATION/calibrate_shaper.py" "$each_file" -o "$PRINTER_DATA/$OUTPUT_FOLDER/$STATIC_NAME"
-        cp "$PRINTER_DATA/$OUTPUT_FOLDER/$STATIC_NAME" "$PRINTER_DATA/$OUTPUT_FOLDER/$HISTORY_NAME"
+        "$KLIPPER_SCRIPTS_LOCATION/calibrate_shaper.py" "$each_file" -o "$BOT_FOLDER/$STATIC_NAME"
+        cp "$BOT_FOLDER/$STATIC_NAME" "$OUTPUT_FOLDER/$HISTORY_NAME"
         rm "$each_file"
     done
 else
-    echo "No CSV files found in $RESONANCE_CSV_LOCATION"
+    echo "No CSV files found to process"
 fi
